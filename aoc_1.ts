4b51412c82af70662a966d075f8ef62b84f379ed
David Kuling <dpkuling@gmail.com>
2024-12-04 00:28:57 +0000

    Initial commit

---
+++ aoc_1.ts
@@ -0,0 +1,31 @@
+const inputFile =  Bun.file("input_1.txt");
+
+let listString: string = await inputFile.text();
+let listPair: string[] = listString.split("\n");
+
+let list1: number[] = [];
+let list2: number[] = [];
+
+for (let i in listPair) {
+    list1.push(Number(listPair[i].split("   ")[0]));
+    list2.push(Number(listPair[i].split("   ")[1]));
+}
+
+function aoc_1(lista:number[], listb:number[]): string {
+    //sort lists
+    let sort_a: number[] = lista.sort();
+    let sort_b: number[] = listb.sort();
+
+    let totalDiff: number = 0;
+    let simScore: number = 0;
+    
+    //iterate over sorted arrays, then add diff to total, and calculate a 'similarity score'
+    for (let i in sort_a) {
+        totalDiff += Math.abs(sort_a[i]-sort_b[i]);
+        simScore += sort_b.filter(x => x===sort_a[i]).length*sort_a[i];
+    }
+
+    return `Total difference: ${totalDiff}\nSimilarity score: ${simScore}`;
+};
+
+console.log(aoc_1(list1, list2));
---
+++ aoc_2.ts
@@ -0,0 +1,55 @@
+let input: string = await Bun.file("input_2.txt").text();
+
+let reports: string[] = input.split("\n");
+
+const enum dir {
+    none,
+    ascending,
+    descending,
+}
+
+function countSafe(list: string[]): string {
+    let safeCount: number = 0;
+    let dampenedSafeCount: number = 0;
+
+    //Take a string, then check if it is safe (asc/desc by >=1 and <=3) AND all ascending / descending
+    for (let i = 0; i<list.length; i++) {
+        let test: number[] = list[i].split(" ").map(Number);
+        if (testArray(test)) safeCount++;
+        else {
+            //Try again, but with dampened arrays
+            for (let j = 0; j<test.length; j++) {
+                let splicedArray: number[] = test.slice(0)
+                splicedArray.splice(j,1);
+                if (testArray(splicedArray)) {
+                    dampenedSafeCount++;
+                    break;
+                }
+            }
+        }
+    }
+    return `Safe reports: ${safeCount}\nDampened safe reports: ${dampenedSafeCount}\nTotal: ${safeCount+dampenedSafeCount}`;
+}
+
+//Test if an array is 'safe'
+function testArray(t: number[]): boolean {
+    let direction: dir = dir.none;
+    for (let j = 0; j < t.length-1; j++) {
+        let diff: number = Math.abs(t[j]-t[j+1])
+        if (diff >=1 && diff<=3) { //Test the difference, reject if 0 or >3
+            if (t[j]>t[j+1] && (direction == dir.none || direction == dir.descending)) {
+                direction = dir.descending;
+                continue;
+            }
+            else if (t[j]<t[j+1] && (direction == dir.none || direction == dir.ascending)) {
+                direction = dir.ascending;
+                continue;
+            }
+            else return false;
+        }
+        else return false;
+    }
+    return true;
+}
+
+console.log(countSafe(reports));
